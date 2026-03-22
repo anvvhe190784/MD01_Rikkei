3e2e4a3987c5b7f803f5ff21ba156a20f6882ccf
Vũ Văn An <vananvu249@gmail.com>
2026-03-22 19:42:41 +0700

    Add files via upload

---
+++ MD01_SS03_Bai7.sql
@@ -0,0 +1,19 @@
+create database "LibraryDB";
+create schema sales;
+create table sales.Products (
+    product_id serial primary key,
+    product_name varchar,
+    price numeric(10,2),
+    stock_quantity integer
+);
+create table sales.Orders (
+    order_id serial primary key,
+    order_date date default current_date,
+    member_id integer references library.Members(member_id)
+);
+create table sales.OrderDetails (
+    order_detail_id serial primary key,
+    order_id integer references sales.Orders(order_id),
+    product_id integer references sales.Products(product_id),
+    quantity integer
+);
