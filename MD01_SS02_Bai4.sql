690bb3283196c77105459eaf4523ad7c98dd1fe2
Vũ Văn An <vananvu249@gmail.com>
2026-03-22 18:51:34 +0700

    Add files via upload

---
+++ MD01_SS02_Bai4.sql
@@ -0,0 +1,44 @@
+create database "ElearningDB";
+create schema elearning;
+create table elearning.Students (
+    student_id serial primary key,
+    first_name varchar(50) not null,
+    last_name varchar(50) not null,
+    email varchar unique not null
+);
+create table elearning.Instructors (
+    instructor_id serial primary key,
+    first_name varchar(50) not null,
+    last_name varchar(50) not null,
+    email varchar unique not null
+);
+create table elearning.Courses (
+    course_id serial primary key,
+    course_name varchar(100) not null,
+    instructor_id int,
+    foreign key (instructor_id) references elearning.Instructors(instructor_id)
+);
+create table elearning.Enrollments (
+    enrollment_id serial primary key,
+    student_id int,
+    course_id int,
+    enroll_date date not null,
+    foreign key (student_id) references elearning.Students(student_id),
+    foreign key (course_id) references elearning.Courses(course_id)
+);
+create table elearning.Assignments (
+    assignment_id serial primary key,
+    course_id int,
+    title varchar(100) not null,
+    due_date date not null,
+    foreign key (course_id) references elearning.Courses(course_id)
+);
+create table elearning.Submissions (
+    submission_id serial primary key,
+    assignment_id int,
+    student_id int,
+    submission_date date not null,
+    grade real check (grade >= 0 and grade <= 100),
+    foreign key (assignment_id) references elearning.Assignments(assignment_id),
+    foreign key (student_id) references elearning.Students(student_id)
+);
