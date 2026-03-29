d57afce67435e554a6db8fcc47b452c31278a8a5
Vũ Văn An <vananvu249@gmail.com>
2026-03-29 18:37:42 +0700

    Add files via upload

---
+++ MD01_SS05_Bai2.sql
@@ -0,0 +1,17 @@
+select p.product_name, sum(o.total_price) as total_revenue
+from products p
+    join orders o on p.product_id = o.product_id
+group by p.product_id, p.product_name
+having sum(o.total_price) = (
+    select max(total_revenue)
+    from (
+        select sum(total_price) as total_revenue
+            from orders
+            group by product_id
+         )
+);
+
+select p.category, sum(o.total_price) as total_revenue
+from products p
+    join orders o on p.product_id = o.product_id
+group by p.category;
