b8c2f07b52a43b59c06be139b86b1bb66e5fc1e5
Vũ Văn An <vananvu249@gmail.com>
2026-04-07 21:32:47 +0700

    Update MD01_SS10_Bai1.sql

--- MD01_SS10_Bai1.sql
+++ MD01_SS10_Bai1.sql
@@ -1,1 +1,30 @@
-CREATE DATABASE user_management;
+create table products (
+                          id serial primary key,
+                          name varchar(100),
+                          price numeric,
+                          last_modified timestamp default current_timestamp
+);
+
+create or replace function update_last_modified()
+    returns trigger
+    language plpgsql
+as $$
+begin
+    new.last_modified := current_timestamp;
+    return new;
+end;
+$$;
+
+create trigger trg_update_last_modified
+    before update on products
+    for each row
+execute function update_last_modified();
+
+insert into products (name, price)
+values ('laptop', 1500);
+
+update products
+set price = 1400
+where name = 'laptop';
+
+select * from products;
